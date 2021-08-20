537340b6fbf437679a8aff12e29917e1c65b0598
SasakiHayato0605 <zuomusundou685@gmail.com>
2021-08-20 18:53:09 +0900

    Fieldの生成

--- Assets/Script/BingoManager.cs
+++ Assets/Script/BingoManager.cs
@@ -1,35 +1,88 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class BingoManager : MonoBehaviour
 {
-    [SerializeField] private GameObject bingoPrefab;
-    [SerializeField] private GridLayoutGroup m_group;
+    const int m_wide = 5;
+    const int m_height = 5;
+    [SerializeField] GameObject m_cell;
 
-    const int row = 5;
-    const int column = 5;
+    CellClass[,] m_cells = new CellClass[m_wide, m_height];
 
-    private Cell[,] cells = new Cell[row, column];
+    List<int> m_setNum = new List<int>();
 
     void Start()
     {
-        for (int i = 0; i < row; i++)
+        CreateCell();
+    }
+
+    void CreateCell()
+    {
+        for (int x = 0; x < m_wide; x++)
         {
-            for (int j = 0; j < column; j++)
+            for (int y = 0; y < m_height; y++)
             {
-                CreateCell(i, j, cells);
+                GameObject cell = Instantiate(m_cell, transform);
+                cell.name = $"cell :{x} :{y}";
+                m_cells[x, y] = FindObjectOfType<CellClass>();
+                if (x == 2 && y == 2)
+                {
+                    cell.GetComponent<CellClass>().SetText("B");
+                    m_cells[x, y].SetStatus(CellStatus.Open);
+                }
+                else
+                {
+                    cell.GetComponent<CellClass>().SetText(SetNum(y).ToString());
+                    m_cells[x, y].SetStatus(CellStatus.Close);
+                }
             }
         }
     }
 
-    private void CreateCell(int x, int y, object[,] cells)
+    int SetNum(int y)
     {
-        var cell = Instantiate(bingoPrefab);
-        var parent = m_group.gameObject.transform;
-        cell.transform.SetParent(parent);
-
-        cells[x, y] = cell;
+        int num = 0;
+        switch (y)
+        {
+            case 0:
+                num = Random.Range(1, 16);
+                break;
+            case 1:
+                num = Random.Range(15, 31);
+                break;
+            case 2:
+                num = Random.Range(30, 46);
+                break;
+            case 3:
+                num = Random.Range(45, 61);
+                break;
+            case 4:
+                num = Random.Range(60, 76);
+                break;
+        }
+        int set = CheckNum(num, y);
+        return set;
+    }
+    int CheckNum(int num, int select)
+    {
+        if (m_setNum.Count == 0)
+        {
+            m_setNum.Add(num);
+            return num;
+        }
+        else
+        {
+            foreach (int checkNum in m_setNum)
+            {
+                if (checkNum == num)
+                {
+                    int set = SetNum(select);
+                    return num = set;
+                }
+            }
+            m_setNum.Add(num);
+        }
+        return num;
     }
 }
---
+++ Assets/Script/CellClass.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum CellStatus
+{
+    Open,
+    Close,
+}
+
+public class CellClass : MonoBehaviour
+{
+    [SerializeField] Text m_numText;
+    CellStatus m_status;
+
+    public void SetText(string set) { m_numText.text = $"{set}"; }
+    public void SetStatus(CellStatus status)
+    { 
+        m_status = status;
+        ChengeColor();
+    }
+    public CellStatus RetuneStatus() { return m_status; }
+
+    void ChengeColor()
+    {
+        Image imageColer = GetComponent<Image>();
+        if (m_status == CellStatus.Close) { imageColer.color = Color.red; }
+        else if (m_status == CellStatus.Open) { imageColer.color = Color.green; }
+    }
+}
